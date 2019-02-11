03b4c4981b8e59aab1c42bb0db098f2f777c5fa1
Lindokuhle Dlamini <DLMLIN017@myuct.ac.za>
2019-02-11 10:47:14 +0200

    restructured code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-let express = require('express')
-let app = express()
-let path = require('path')
-let bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
 const db = require('./db.js');
 const mongoose = require('mongoose');
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+const PORT= process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-
 mongoose.connect(db.url, {useNewUrlParser: true}).then(() => {
-    console.log('DB connected');
-}).catch(err => {
-    console.log('Could not connect to Database. error..', err);
-    process.exit();
-})
+        console.log('DB connected');
+    }).catch(err => {
+        console.log('Could not connect to Database. error..', err);
+        process.exit();
+    }
+);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.sendFile(path.join(__dirname, '../public/500.html'))
-})
+    console.error(err.stack);
+    res.sendFile(path.join(__dirname, '../public/500.html'));
+});
 
-const PORT= process.env.PORT || 3000
 require('./routes/user.js')(app);
 require('./routes/post.js')(app);
-app.listen(PORT, () => console.info(`Server running on ${PORT}`))
+app.listen(PORT, () => console.info(`Server running on ${PORT}`));
